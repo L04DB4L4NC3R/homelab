3c8df891fa544a6c914d376ae7370502f5436123
Angad Sharma <angadsharma1016@gmail.com>
2020-10-24 22:56:23 +0000

    breaking down the configs

--- configure.sh
+++ configure.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# HomeLab Main
-docker stack deploy -c homelab.yml homelab
+# Music Sync
+docker stack deploy -c homelab.yml music-sync.yml
 
 # NextCloud Stack
 docker stack deploy -c nextcloud.yml nextcloud
 
+# Telegram servers
+docker stack deploy -c telegram-servers.yml telegram-servers
+
 # Pihole Install
 curl -sSL https://install.pi-hole.net | bash
+
