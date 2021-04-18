d9ef9424a4b5925380db4d599b03dec3ba6077a5
Angad Sharma <30529572+L04DB4L4NC3R@users.noreply.github.com>
2021-04-18 14:56:52 +0530

    Update music-streaming-server-polaris.sh

--- scripts/music-streaming-server-polaris.sh
+++ scripts/music-streaming-server-polaris.sh
@@ -4,6 +4,6 @@ docker run -t \
   -v /mnt/hdd/Music:/music \
   -v cache:/var/cache/polaris \
   -v data:/var/lib/polaris \
-  -d \
+  -d --restart always\
   angadsharma1016/polaris:arm64 # for armv8
   #ogarcia/polaris:latest # for x86
